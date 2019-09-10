11320ca66beb519163381cd02e2ef4e9978bd062
Tim Grant <tim.grant.90014@gmail.com>
2019-09-10 09:51:33 -0700

    misc

--- src/components/Form/Panels/CandidateOrBallotMeasure/CandidateOrBallotMeasure.js
+++ src/components/Form/Panels/CandidateOrBallotMeasure/CandidateOrBallotMeasure.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react'
+import React, { Component, useState } from 'react'
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import FormLabel from "@material-ui/core/FormLabel"
 import Radio from "@material-ui/core/Radio"
@@ -9,7 +9,6 @@ import BallotMeasure from "../../../Form/Group/BallotMeasure/BallotMeasure"
 import Candidate from "../../../Form/Group/Candidate/Candidate"
 
 //need to take in initialization value. also needs to pass on selected props. spinner happens at candidate or ballot measure level?
-
 class CandidateOrBallotMeasure extends Component {
 
     state = {
@@ -17,52 +16,59 @@ class CandidateOrBallotMeasure extends Component {
     }
     //if is Candidate show candidate prop, else, show ballotmeasure. But handle loading. 
 
-    render() {    
-        const [isCandidate, setIsCandidate] = useState(true)
-        const showCandidateOrBallotmeasure = isCandidate ? (
-            <div><Candidate /></div>
-        ) : (
-            <div><BallotMeasure /></div>
+    setIsCandidate = isCandidate => {
+        this.setState({
+            isCandidate: isCandidate
+        })
+    }
+
+    render() {
+
+        const showCandidateOrBallotmeasure = (
+            this.state.isCandidate ? (
+                <div><Candidate /></div>
+            ) : (
+                <div><BallotMeasure /></div>
+                )
         )
+
         return (
             //have radio button here
             //which switches between candidate or ballot measure. could be a function maybe? but needs state
             // <Candidate {...this.props} />					
             <FormControl component="fieldset">
-					<FormLabel component="legend">
-                        Choose Candidate or Ballot Measure
+                <FormLabel component="legend">
+                    Choose Candidate or Ballot Measure
                     </FormLabel>
-                    <Field name="subject" component={renderRadioGroup} row>
-                        <FormControlLabel
-                            value="C"
-                            control={
-                                <Radio
-                                    color="primary"
-                                    onClick={() => setIsCandidate(true)}
-                                />
-                            }
-                            label="Candidate"
-                            labelPlacement="end"
-                        />
-                        <FormControlLabel
-                            value="B"
-                            control={
-                                <Radio
-                                    color="primary"
-                                    onClick={() => setIsCandidate(false)}
-                                />
-                            }
-                            label="Ballot Measure"
-                            labelPlacement="end"
-                        />
-                    </Field>
-                    {showCandidateOrBallotmeasure}
-                </FormControl>
+                <Field name="subject" component={renderRadioGroup} row>
+                    <FormControlLabel
+                        value="C"
+                        control={
+                            <Radio
+                                color="primary"
+                                onClick={() => this.setIsCandidate(true)}
+                            />
+                        }
+                        label="Candidate"
+                        labelPlacement="end"
+                    />
+                    <FormControlLabel
+                        value="B"
+                        control={
+                            <Radio
+                                color="primary"
+                                onClick={() => this.setIsCandidate(false)}
+                            />
+                        }
+                        label="Ballot Measure"
+                        labelPlacement="end"
+                    />
+                </Field>
+                {showCandidateOrBallotmeasure}
+            </FormControl>
 
         )
     }
 }
-  
-    
 
 export default CandidateOrBallotMeasure
