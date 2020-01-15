82864990e56372ca95090f275e05e6776f50a5c8
ken-erickson <ken.erickson@lacity.org>
2020-01-15 13:56:51 -0800

    Alpha 1.0.3 - Fixed more icon button

--- src/components/UI/Paper/Hooks/useExpandClick.js
+++ src/components/UI/Paper/Hooks/useExpandClick.js
@@ -7,9 +7,6 @@ import { makeStyles } from "@material-ui/core/styles"
 
 
 const useStyles = makeStyles(theme => ({
-	grid: {
-		marginBottom: theme.spacing(3)
-	},
 
 	expand: {
 		transform: "rotate(0deg)",
